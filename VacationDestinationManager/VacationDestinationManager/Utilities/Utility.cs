9fa94456b07587d7301a13fe1d3e8124709cba3a
Alin Stanciu <alinstanciu2011@yahoo.com>
2023-05-21 14:50:22 +0300

    admin-done-bugfix

--- VacationDestinationManager/VacationDestinationManager/Utilities/Utility.cs
+++ VacationDestinationManager/VacationDestinationManager/Utilities/Utility.cs
@@ -1,11 +1,17 @@
-﻿namespace VacationDestinationManager.Utilities
+﻿using System.Drawing.Imaging;
+
+namespace VacationDestinationManager.Utilities
 {
     internal class Utility
     {
-        public static byte[] ImageToBytes(Image image)
+        public static byte[] ImageToBytes(Image? image)
         {
+            if (image is null)
+                return Array.Empty<byte>();
+
             using var memoryStream = new MemoryStream();
-            image.Save(memoryStream, image.RawFormat);
+            using var tempImage = new Bitmap(image);
+            tempImage.Save(memoryStream, ImageFormat.Jpeg);
             return memoryStream.ToArray();
         }
     }
